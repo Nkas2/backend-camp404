a499de77c75e2bf38c2d8cb7affdc53b4ae48907
Nayandra <kastoronayandra@gmail.com>
2023-06-04 09:49:16 +0700

    lupa commit

---
+++ model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema(
+   {
+      judul : {type: String, require: true},
+      pengarang : {type: String,  require: true}
+   },
+   {collection: 'koleksiBuku'}
+)
+
+module.exports = mongoose.model('BukuModel', BookSchema);
